cc33d12d09fecf9f59c66e4cfa480179940a2f71
MatthieuL78 <matthieu.lu@outlook.fr>
2018-05-11 00:00:07 +0200

    last

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'matiers/index'
   resources :emails
-  get 'matier/', to: 'matiers#show_matier', as: :my_matier
+  get 'matier/:id', to: 'matiers#show_matier', as: :my_matier
   post '/matier/', to: 'landingpages#get_matier', as: :get_matier
+  post '/my_index/', to: 'matiers#index', as: :my_index
   root 'landingpages#home'
 end
